5a2532b705f3f33ed109b9155390b72d3f24e87d
Anish Gupta <anishgupta675@gmail.com>
2023-09-18 23:44:01 +0530

    The K Weakest Rows in a Matrix

--- Daily/The_K_Weakest_Rows_in_a_Matrix/main.cpp
+++ Daily/The_K_Weakest_Rows_in_a_Matrix/main.cpp
@@ -2,28 +2,15 @@
 
 using namespace std;
 
-struct comp {
-    constexpr bool operator() (
-    pair<int, int> const &a,
-    pair<int, int> const &b)
-    const noexcept {
-        if(a.first > b.first) return true;
-        if(a.first < b.first) return false;
-        return a.second > b.second;
-    }
-};
-
 class Solution {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        vector<int> res(k);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, comp> pq;
-        for(int i = 0; i < mat.size(); i++) pq.push(make_pair(upper_bound(mat[i], mat[i] + mat[i].size(), 1, greater<int>()), i));
-        for(int i = 0; i < k; i++) {
-            res[i] = pq.top().second;
-            pq.pop();
-        }
-        return res;
+        vector<int> result;
+        vector<pair<int, int>> rowStrengths;
+        for(int i = 0; i < mat.size(); ++i) rowStrengths.push_back({accumulate(mat[i].begin(), mat[i].end(), 0), i});
+        sort(rowStrengths.begin(), rowStrengths.end());
+        for(int i = 0; i < k; ++i) result.push_back(rowStrengths[i].second);
+        return result;
     }
 };
 
