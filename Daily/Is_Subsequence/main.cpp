e259a5068e02e250fa88c20d2bab4889af1ff9c8
Anish Gupta <anishgupta675@gmail.com>
2023-09-25 12:55:20 +0530

    Is Subsequence

---
+++ Daily/Is_Subsequence/main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int j = 0;
+        for(int i = 0; i < t.length() && j < s.length(); i++) if(s[j] == t[i]) j++;
+        return j == s.length();
+    }
+};
+
+int main() {}
