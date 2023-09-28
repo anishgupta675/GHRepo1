ab1d684526d4cb7ec3f220c9b27b86cb8c247df5
Anish Gupta <anishgupta675@gmail.com>
2023-09-28 14:53:42 +0530

    Find the Difference

--- Daily/Find_the_Difference/main.cpp
+++ Daily/Find_the_Difference/main.cpp
@@ -5,10 +5,6 @@ using namespace std;
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        // unordered_set<char> uset;
-        // for(int i = 0; i < s.length(); i++) uset.insert(s[i]);
-        // for(int i = 0; i < t.length(); i++) if(uset.find(t[i]) == uset.end()) return t[i];
-        // return '\0';
         unordered_map<char, int> umap;
         for(int i = 0; i < s.length(); i++) umap[s[i]]++;
         for(int i = 0; i < t.length(); i++) {
