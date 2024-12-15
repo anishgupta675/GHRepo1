f754ce5dd39f78879811f70598ec113213998387
Anish Gupta <anishgupta675@gmail.com>
2024-12-15 18:28:38 +0000

    Maximum Average Pass Ratio

---
+++ Daily/Maximum_Average_Pass_Ratio/main.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxAverageRatio(self, classes: List[List[int]], k: int) -> float:
+        n, total, a = len(classes), 0, []
+        for p,q in classes:
+            total += p / q
+            a.append(((p - q) / (q * (q + 1)), p, q))
+        heapify(a)
+        for i in range(k):
+            r, p, q = a[0]
+            if not r:
+                break
+            total -= r
+            r2 = (p - q) / ((q + 1.0) * (q + 2.0))
+            heapreplace(a, (r2, p + 1, q + 1))
+        return total / n
