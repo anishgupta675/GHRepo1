2b602520e5804eca23f173ba5a36ef7ba880a95c
Anish Gupta <anishgupta675@gmail.com>
2024-12-16 17:42:59 +0000

    Final Array State After K Multiplication Operations I

---
+++ Daily/Final_Array_State_After_K_Multiplication_Operations_I/main.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int):
+        pq = [(val, i) for i, val in enumerate(nums)]
+        heapify(pq)
+        for i in range(k):
+            i, j = heappop(pq)
+            nums[j] *= multiplier
+            heappush(pq, (nums[j], j))
+        return nums
