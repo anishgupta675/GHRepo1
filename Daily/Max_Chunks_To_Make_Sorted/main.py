919a5546a2fa658cf6aa61b3a6687724c40572a1
Anish Gupta <anishgupta675@gmail.com>
2024-12-19 16:39:38 +0000

    Max Chunks To Make Sorted

---
+++ Daily/Max_Chunks_To_Make_Sorted/main.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        max_val, count = 0, 0
+        for i in range(len(arr)):
+            max_val = max(arr[i], max_val)
+            if i == max_val:
+                count += 1
+        return count
