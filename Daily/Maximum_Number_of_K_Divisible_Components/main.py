7030dce0fb9e3959f9f06f20c78e237e54b2438d
Anish Gupta <anishgupta675@gmail.com>
2024-12-21 15:12:16 +0000

    Maximum Number of K-Divisible Components

---
+++ Daily/Maximum_Number_of_K_Divisible_Components/main.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
+        adj = defaultdict(list)
+        for i, j in edges:
+            adj[i].append(i)
+            adj[j].append(i)
+        res = 0
+        def dfs(cur, parent):
+            total = values[cur]
+            for child in adj[cur]:
+                if child != parent:
+                    total += dfs(child, cur)
+            if total % k == 0:
+                nonlocal res
+                res += 1
+            return total
+        dfs(0, -1)
+        return res
