a972bbfedc53173b81f7e373419f6622c2223c05
Anish Gupta <anishgupta675@gmail.com>
2024-12-27 08:27:07 +0000

    Best Sightseeing Pair

---
+++ Daily/Best_Sightseeing_Pair/main.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        res, cur_max = 0, values[0] - 1
+        for i in range(1, len(values)):
+            res = max(res, values[i] + cur_max)
+            cur_max = max(cur_max - 1, values[i] - 1)
+        return res
