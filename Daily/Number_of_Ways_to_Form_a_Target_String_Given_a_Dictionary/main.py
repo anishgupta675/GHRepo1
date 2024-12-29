c2de90b5cd8847a545f9e8eaa2a6cf7b1ebe718b
Anish Gupta <anishgupta675@gmail.com>
2024-12-29 07:45:24 +0000

    Number of Ways to Form a Target String Given a Dictionary

---
+++ Daily/Number_of_Ways_to_Form_a_Target_String_Given_a_Dictionary/main.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numWays(self, words: List[str], target: str) -> int:
+        mod, dp = (10 ** 9) + 7, [0] * (len(target) + 1)
+        dp[0] = 1
+        count = [[0] * 26] * len(words[0])
+        for word in words:
+            for i in range(len(words[0])):
+                count[i][ord(word[i]) - ord('a')] += 1
+        for i in range(len(words[0])):
+            for j in range(len(target) - 1, -1, -1):
+                dp[j + 1] += (dp[j] * count[i][ord(target[j]) - ord('a')]) % mod
+                dp[j + 1] %= mod
+        return dp[len(target)]
