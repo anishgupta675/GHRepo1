055e87379fde5550330d3969c370d8bef2ba265c
Anish Gupta <anishgupta675@gmail.com>
2024-12-31 05:46:12 +0000

    Minimum Cost For Tickets

---
+++ Daily/Minimum_Cost_For_Tickets/main.py
@@ -0,0 +1,11 @@
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        dp, travel_days = [0] * 366, set()
+        for i in days:
+            travel_days.add(i)
+            for i in range(1, 366):
+                if i in travel_days:
+                    dp[i] = dp[i - 1]
+                else:
+                    dp[i] = min(dp[max(0, i - 7)] + costs[1], dp[max(0, i - 30)] + costs[2], dp[i - 1] + costs[0])
+        return dp[365]
