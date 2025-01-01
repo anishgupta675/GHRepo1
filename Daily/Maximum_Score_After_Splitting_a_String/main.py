022426ea57bae66d83c92381169ffb291224e0a7
Anish Gupta <anishgupta675@gmail.com>
2025-01-01 14:39:26 +0000

    Maximum Score After Splitting a String

---
+++ Daily/Maximum_Score_After_Splitting_a_String/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        ones, zeroes, max_val = 0, 0, -inf
+        for i in range(len(s) - 1):
+            if s[i] == "1":
+                ones += 1
+            else:
+                zeroes += 1
+            max_val = max(max_val, zeroes - ones)
+        if s[-1] == "1":
+            ones += 1
+        return max_val + ones
