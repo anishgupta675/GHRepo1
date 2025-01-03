eaf83c2966aea1aed24e3b0b8bb59891a43425a6
Anish Gupta <anishgupta675@gmail.com>
2025-01-03 16:53:53 +0000

    Number of Ways to Split Array

---
+++ Daily/Number_of_Ways_to_Split_Array/main.py
@@ -0,0 +1,8 @@
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        pref_sum = [0] * len(nums)
+        pref_sum[0] = nums[0]
+        for i in range(1, len(nums)):
+            pref_sum[i] = pref_sum[i - 1] + nums[i]
+        count = sum(1 for i in range(len(nums) - 1) if pref_sum[i] >= pref_sum[-1] - pref_sum[i])
+        return count
