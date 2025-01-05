8e75bdeebf4c7b788f429ced71208d9f1cab8c4a
Anish Gupta <anishgupta675@gmail.com>
2025-01-05 10:51:36 +0000

    Shifting Letters II

---
+++ Daily/Shifting_Letters_II/main.py
@@ -0,0 +1,21 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        diff = [0] * len(s)
+        for i in shifts:
+            if i[2] == 1:
+                diff[i[0]] += 1
+                if i[1] + 1 < len(s):
+                    diff[i[1] + 1] -= 1
+            else:
+                diff[i[0]] -= 1
+                if i[1] + 1 < len(s):
+                    diff[i[1] + 1] += 1
+        res = list(s)
+        count = 0
+        for i in range(len(s)):
+            count = (count + diff[i]) % 26
+            if count < 0:
+                count += 26
+            char = chr((ord(s[i]) - ord("a") + count) % 26 + ord("a"))
+            res[i] = char
+        return "".join(res)
