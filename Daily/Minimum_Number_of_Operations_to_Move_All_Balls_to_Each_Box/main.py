4d343e2cf56d4e0a9b3d48ee1f369f88d36e56af
Anish Gupta <anishgupta675@gmail.com>
2025-01-06 16:28:22 +0000

    Minimum Number of Operations to Move All Balls to Each Box

---
+++ Daily/Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        ans = [0] * len(boxes)
+        b1, m1, b2, m2 = 0, 0, 0, 0
+        for i in range(len(boxes)):
+            ans[i] += m1
+            b1 += int(boxes[i])
+            m1 += b1
+            ans[len(boxes) - 1 - i] += m2
+            b2 += int(boxes[len(boxes) - 1 - i])
+            m2 += b2
+        return ans
