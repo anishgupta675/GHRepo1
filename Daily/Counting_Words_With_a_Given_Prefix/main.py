2e532819b2d284608faf4ef09626166996e5c120
Anish Gupta <anishgupta675@gmail.com>
2025-01-09 16:47:56 +0000

    Counting Words With a Given Prefix

---
+++ Daily/Counting_Words_With_a_Given_Prefix/main.py
@@ -0,0 +1,7 @@
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        f = 0
+        for i in range(len(words)):
+            if words[i].startswith(pref):
+                f += 1
+        return f
