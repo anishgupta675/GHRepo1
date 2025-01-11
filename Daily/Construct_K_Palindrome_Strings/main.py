44a5de6c9f54e3121b88762d7ab21e1e60eb3499
Anish Gupta <anishgupta675@gmail.com>
2025-01-11 07:09:52 +0000

    Construct K Palindrome Strings

---
+++ Daily/Construct_K_Palindrome_Strings/main.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        if len(s) < k:
+            return False
+        if len(s) == k:
+            return True
+        odd = 0
+        for i in s:
+            odd ^= 1 << (ord(i) - ord("a"))
+        return bin(odd).count("1") <= k
