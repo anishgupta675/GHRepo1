30e7a786103debf1ad5e600800a7967d5f063848
Anish Gupta <anishgupta675@gmail.com>
2025-01-16 16:42:40 +0000

    Bitwise XOR of All Pairings

---
+++ Daily/Bitwise_XOR_of_All_Pairings/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
+        freq = {}
+        for i in nums1:
+            freq[i] = freq.get(i, 0) + len(nums2)
+        for i in nums2:
+            freq[i] = freq.get(i, 0) + len(nums1)
+        ans = 0
+        for i in freq:
+            if freq[i] % 2:
+                ans ^= i
+        return ans
