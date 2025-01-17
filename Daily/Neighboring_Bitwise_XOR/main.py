701c20162a38772f8de1aab07c6fa03ec389a8da
Anish Gupta <anishgupta675@gmail.com>
2025-01-17 14:21:15 +0000

    Neighboring_Bitwise_XOR

---
+++ Daily/Neighboring_Bitwise_XOR/main.py
@@ -0,0 +1,7 @@
+class Solution:
+    def doesValidArrayExist(self, derived: List[int]) -> bool:
+        count = 0
+        for i in derived:
+            if i == 1:
+                count += 1
+        return not count % 2
