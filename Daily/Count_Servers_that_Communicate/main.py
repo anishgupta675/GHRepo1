d5536dddb90028ac013895fe972d4b9b3a5c1dad
Anish Gupta <anishgupta675@gmail.com>
2025-01-23 16:23:11 +0000

    Count Servers that Communicate

---
+++ Daily/Count_Servers_that_Communicate/main.py
@@ -0,0 +1,5 @@
+class Solution:
+    def countServers(self, grid: List[List[int]]) -> int:
+        r = [sum(i) for i in grid]
+        c = [sum(grid[i][j] for i in range(len(grid))) for j in range(len(grid[0]))]
+        return sum(1 for i in range(len(grid)) for j in range(len(grid[0])) if grid[i][j] == 1 and (r[i] > 1 or c[j] > 1))
