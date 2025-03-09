6fa6cec98bf3002b35c883144e37d9af5fa5e51b
Anish Gupta <anishgupta675@gmail.com>
2025-03-09 17:30:42 +0000

    Fruits Into Baskets II

---
+++ Contests/Weekly_Contest_440/Fruits_Into_Baskets_II/main.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
+        n, count, placed_fruits, placed_baskets = len(fruits), 0, set(), set()
+        for i in range(len(fruits)):
+            for j in range(len(baskets)):
+                if fruits[i] <= baskets[j] and i not in placed_fruits and j not in placed_baskets:
+                    placed_fruits.add(i)
+                    placed_baskets.add(j)
+                    count += 1
+        return n - len(placed_fruits)
