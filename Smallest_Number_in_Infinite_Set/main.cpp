9818c033560cee9df6506237db3b183809e1923f
Anish Gupta <anishgupta675@gmail.com>
2025-03-10 17:51:41 +0000

    Smallest Number in Infinite Set

---
+++ Smallest_Number_in_Infinite_Set/main.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class SmallestInfiniteSet {
+    int cur;
+    set<int> heap;
+public:
+    SmallestInfiniteSet() {
+        cur = 0;
+    }
+    
+    int popSmallest() {
+        if(!heap.size()) {
+            int smallest = *heap.begin();
+            heap.erase(heap.begin());
+            return smallest;
+        }
+        cur++;
+        return cur - 1;
+    }
+    
+    void addBack(int num) {
+        if(num < cur) heap.insert(num);
+    }
+};
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet* obj = new SmallestInfiniteSet();
+ * int param_1 = obj->popSmallest();
+ * obj->addBack(num);
+ */
+
+int main() {}
