f9740b953ec0a131258e05127ca1d1679dce0c4a
Anish Gupta <anishgupta675@gmail.com>
2025-03-17 16:41:34 +0000

    Divide Array Into Equal Pairs

---
+++ Daily/Divide_Array_Into_Equal_Pairs/main.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_map<int, int> frequency;
+        for (int num : nums) {
+            frequency[num]++;
+        }
+        for (auto& entry : frequency) {
+            if (entry.second % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main() {}
