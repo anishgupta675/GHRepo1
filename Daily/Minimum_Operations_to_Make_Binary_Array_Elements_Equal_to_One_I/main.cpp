36de3da07852a5f4fac166378a4cdb54f1193d45
Anish Gupta <anishgupta675@gmail.com>
2025-03-19 16:03:47 +0000

    Minimum Operations to Make Binary Array Elements Equal to One I

---
+++ Daily/Minimum_Operations_to_Make_Binary_Array_Elements_Equal_to_One_I/main.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int count = 0;
+        deque<int> flipQueue;
+        for(int i = 0; i < nums.size(); i++) {
+            while(!flipQueue.empty() && i > flipQueue.front() + 2)
+                flipQueue.pop_front();
+            if ((nums[i] + flipQueue.size()) % 2 == 0) {
+                if (i + 2 >= nums.size()) return -1;
+                count++;
+                flipQueue.push_back(i);
+            }
+        }
+        return count;
+    }
+};
+
+int main() {}
