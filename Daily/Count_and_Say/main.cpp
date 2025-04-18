50ebce1499c8db8febf46012e8ec372886b28a81
Anish Gupta <anishgupta675@gmail.com>
2025-04-18 18:20:43 +0000

    Count and Say

---
+++ Daily/Count_and_Say/main.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        string res {"1"};
+        for (int i = 2; i <= n; ++i) {
+            res = countAndSayHelper(res);
+        }
+        return res;
+    }
+private:
+    string countAndSayHelper(const string& str) {
+        string res;
+        for (int i = 0; i < str.size(); ++i) {
+            int count = 1;
+            while (i < str.size() -1 && str[i] == str[i+1]) {
+                count++;
+                i++;
+            }
+            res += to_string(count) + str[i];
+        }
+        return res;
+    }
+};
+
+int main() {}
