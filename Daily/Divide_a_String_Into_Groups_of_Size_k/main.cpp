585adb8aad4a4defda380262aede70b5264c2dc0
Anish Gupta <anishgupta675@gmail.com>
2025-06-22 21:41:19 +0530

    Divide a String Into Groups of Size k

---
+++ Daily/Divide_a_String_Into_Groups_of_Size_k/main.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<string> divideString(string s, int k, char fill) {
+		vector<string> res;
+		int n = s.size();
+		int curr = 0;
+		while(curr < n) {
+			res.push_back(s.substr(curr, k));
+			curr += k;
+		}
+		res.back() += string(k - res.back().length(), fill);
+		return res;
+	}
+};
+
+int main() {}
