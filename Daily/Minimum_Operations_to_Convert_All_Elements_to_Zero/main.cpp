2894a9ff63451f9ddc4f1fbdfd744a8330a961b2
Anish Gupta <anishgupta675@gmail.com>
2025-11-10 23:18:36 +0530

    Minimum Operations to Convert All Elements to Zero

---
+++ Daily/Minimum_Operations_to_Convert_All_Elements_to_Zero/main.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        vector<int> s;
+        int res = 0;
+        for (int a : nums) {
+            while (!s.empty() && s.back() > a) {
+                s.pop_back();
+            }
+            if (a == 0) continue;
+            if (s.empty() || s.back() < a) {
+                ++res;
+                s.push_back(a);
+            }
+        }
+        return res;
+    }
+};
+
+int main() {}
