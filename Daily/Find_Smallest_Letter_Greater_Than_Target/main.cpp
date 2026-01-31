e95d91cbd5554c1644239047ae7d074eb010620f
Anish Gupta <anishgupta675@gmail.com>
2026-01-31 23:59:03 +0530

    Find Smallest Letter Greater Than Target

---
+++ Daily/Find_Smallest_Letter_Greater_Than_Target/main.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        char res = letters[0];
+        bool flag = false;
+
+        for (char ch : letters) {
+            if (!flag) {
+                if (ch > target) {
+                    res = ch;
+                    flag = !flag;
+                }
+            } else {
+                if (ch > target && ch < res) res = ch;
+            }
+        }
+
+        return res;
+    }
+};
+
+int main() {}
