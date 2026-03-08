c6bc28c0122364b528363e82c58afa09e2a1d947
Anish Gupta <anishgupta675@gmail.com>
2026-03-08 23:47:45 +0530

    Find Unique Binary String

---
+++ Daily/Find_Unique_Binary_String/main.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        string ans;
+        for (int i = 0; i < nums.size(); i++) {
+            char curr = nums[i][i];
+            ans += curr == '0' ? '1' : '0';
+        }
+        
+        return ans;
+    }
+};
+
+int main() {}
