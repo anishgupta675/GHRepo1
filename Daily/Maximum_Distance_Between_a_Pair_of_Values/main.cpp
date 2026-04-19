cca6f5e10e4deb4f196925f964b11f618bd52c3f
Anish Gupta <anishgupta675@gmail.com>
2026-04-19 16:13:47 +0530

    Maximum Distance Between a Pair of Values

---
+++ Daily/Maximum_Distance_Between_a_Pair_of_Values/main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxDistance(vector<int>& A, vector<int>& B) {
+        int i, j;
+
+        for (i = 0, j = 0; i < A.size() && j < B.size(); j++)
+            i += A[i] > B[j];
+
+        return max(0, j - i - 1);
+    }
+};
+
+int main() {}
